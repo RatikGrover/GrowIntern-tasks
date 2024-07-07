a96ce3117b39e0d27f13ca73281b8c3ef8327c8f
ratikgrover <ratikgrover67@gmail.com>
2024-07-08 00:02:09 +0530

    Add files via upload
    Disney Clone completed

---
+++ Task-2 (Disney-Clone)/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Join Sam Wilson and Bucky Barnes as they team up to face new threats and challenges in the post-Endgame world. This action-packed series explores themes of legacy, identity, and heroism, offering a thrilling continuation of the Marvel Cinematic Universe',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Follow the God of Mischief in this mind-bending series as he steps out of his brothers shadow and embarks on a journey that traverses time and space. With unpredictable twists and a fresh perspective on Loki, this series is a must-watch for Marvel fans.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Experience a unique blend of classic television and the Marvel Cinematic Universe with Wanda Maximoff and Vision. This innovative series delves into the mystery of their suburban life, uncovering secrets and showcasing Wandas powerful reality-altering abilities.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Embark on an epic adventure with Raya as she searches for the last dragon to save her kingdom from an ancient evil. This beautifully animated film celebrates courage, trust, and the power of unity in a breathtaking fantasy world.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Dive into a heartwarming tale of friendship and self-discovery set in a charming seaside town on the Italian Riviera. Follow Luca and his best friend Alberto as they experience an unforgettable summer filled with fun, adventure, and the secret of their true identities.',
+        image: 'images/slider 5.PNG'
+    }
+]
